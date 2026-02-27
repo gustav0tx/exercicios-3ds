cf5e4e09d226c66522be0243d0e3cbbf7a0f5bdc
Gustavo Teixeira <teixeiragoesgustavo@gmail.com>
2026-02-27 13:39:24 -0300

    Update index.test.js

--- 1-bimestre/teste-de-software/projetos/projeto1-sistema-de-validacao-de-pedidos/test/index.test.js
+++ 1-bimestre/teste-de-software/projetos/projeto1-sistema-de-validacao-de-pedidos/test/index.test.js
@@ -1,16 +1,16 @@
 const validarPedido = require('../index')
 
-test('O pedido deve ter valor total maior que 0', () => {
+test('Pedido com total 0 deve retornar false', () => {
     const pedido = { cliente: 'guga', total: 0, itens: ['Produto 1'] }
     expect(validarPedido(pedido)).toBeFalsy()
 })
 
-test('O pedido deve possuir a propriedade cliente', () => {
+test('Pedido sem cliente deve retornar false', () => {
     const pedido = { total: 12, itens: ['Produto 1'] }
     expect(validarPedido(pedido)).toBeFalsy()
 })
 
-test('O pedido deve possuir um array itens', () => {
+test('Pedido sem itens deve retornar false', () => {
     const pedido = { cliente: 'guga', total: 123 }
     expect(validarPedido(pedido)).toBeFalsy()
 })
